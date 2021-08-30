137e34dfd4f8c570e57bf4124c6aa9c09b276135
Adam Oommen Jacob <adamoommenjacob@gmail.com>
2021-08-30 20:03:33 +0530

    rows done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Banner from './Components/Banner/Banner';
 import NavBar from './Components/NavBar/NavBar.js';
+import RowPost from './Components/RowPost/RowPost';
 
 function App() {
   return (
     <div >
       <NavBar/>
       <Banner/>
+      <RowPost/>
     </div>
   );
 }
